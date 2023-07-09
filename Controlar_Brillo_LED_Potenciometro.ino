9b4b367fed5a95fdc440d37f75610d950001a37a
Jonathan Lorca <91257407+LorcaJonathan@users.noreply.github.com>
2023-07-09 19:03:57 +0200

    Update Controlar_Brillo_LED_Potenciometro.ino

--- Controlar_Brillo_LED_Potenciometro.ino
+++ Controlar_Brillo_LED_Potenciometro.ino
@@ -1,19 +1,12 @@
+int led = 11;              // Variable que almacena el número de pin del LED
+int brillo;                // Variable que almacena el nivel de brillo
+int potenciometro = 0;     // Variable que almacena el número de pin del potenciómetro
 
-int led = 11;
-int brillo;
-int potenciometro = 0;
-
-void setup() { //las entradas analogicas no requieren inicializacion
-  // put your setup code here, to run once:
-
-  pinMode(led, OUTPUT);
-
+void setup() {
+  pinMode(led, OUTPUT);    // Configura el pin del LED como salida
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  brillo = analogRead(potenciometro) / 4;
-  analogWrite(led, brillo);
-
+  brillo = analogRead(potenciometro) / 4;    // Lee el valor analógico del potenciómetro y lo divide por 4 para ajustarlo al rango de 0 a 255 (niveles de brillo)
+  analogWrite(led, brillo);                   // Establece el nivel de brillo del LED utilizando PWM
 }
